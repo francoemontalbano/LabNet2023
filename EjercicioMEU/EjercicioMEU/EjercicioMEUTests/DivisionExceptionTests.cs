b2bdb1f2fb164c82203e9fb51423fa226f9bc68c
Franco Montalbano <francoemontalbano@gmail.com>
2023-03-08 14:40:29 -0300

    Agrego solucion y archivos del ejercicio 1 modulo 2

---
+++ EjercicioMEU/EjercicioMEU/EjercicioMEUTests/DivisionExceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejercicio1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Tests
+{
+    [TestClass()]
+    public class DivisionExceptionTests
+    {
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            DivisionException divisionException = new DivisionException();
+            int dividendo = 10;
+            divisionException.Division(dividendo);
+        }
+    }
+}
