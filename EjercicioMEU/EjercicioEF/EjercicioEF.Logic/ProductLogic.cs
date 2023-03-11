cc0c5048cd79b9d293d4ce2998f61ca733be2f9b
Franco Montalbano <francoemontalbano@gmail.com>
2023-03-11 14:01:39 -0300

    Agrego solucion y proyectos del modulo EF

---
+++ EjercicioMEU/EjercicioEF/EjercicioEF.Logic/ProductLogic.cs
@@ -0,0 +1,25 @@
+﻿using EjercicioEF.Data;
+using EjercicioEF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEF.Logic
+{
+    public class ProductLogic
+    {
+        private NorthwindContext _northwindContext; 
+
+        public ProductLogic()
+        {
+            _northwindContext = new NorthwindContext();
+        }
+
+        public List<Products> GetAll()
+        {
+            return _northwindContext.Products.ToList();
+        }
+    }
+}
---
+++ EjercicioMEU/EjercicioEF/EjercicioEF.UI/Program.cs
@@ -0,0 +1,28 @@
+﻿using EjercicioEF.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEF.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Products");
+
+            ProductLogic productlogic = new ProductLogic();
+
+            var products = productlogic.GetAll();
+
+            foreach (var prod in productlogic.GetAll())
+            {
+                Console.WriteLine(prod.ProductName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
